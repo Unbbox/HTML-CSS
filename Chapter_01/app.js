a29884330cbe69eb5360ebd8ae54516445dd99b6
unboxx <ehe110894@naver.com>
2024-08-20 10:01:21 +0900

    나열되는 태그

---
+++ Chapter_01/app.js
@@ -0,0 +1,14 @@
+const radio_btns = document.querySelectorAll("[name='radio_btns']");
+const ordered_list = document.querySelector("ol");
+const ordered_start = document.querySelector("#start_num");
+
+radio_btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    ordered_list.type = e.target.value;
+  });
+});
+
+ordered_start.addEventListener("change", (e) => {
+  // console.log(e.target.value);
+  ordered_list.start = e.target.value;
+});
